6bf0f0a11060ba4341758c1734fd08f23c1a1ef6
Therapy <therapyspb@yandex.ru>
2017-08-28 22:18:02 +0300

    added susbmit() - not finished some fixes in MyFormFields fixed checkValidity.fio whitespaces

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
-const MyFormFields = {
+let MyFormFields = {
   form: document.getElementById('myForm'),
   fio: document.getElementById('fio'),
   email: document.getElementById('email'),
-  phone: document.getElementById('phone')
+  phone: document.getElementById('phone'),
+  submit: document.getElementById('submitButton')
 };
 
-form.setAtttibute('novalidate', true);
+// отключение нативной валидации форм
+MyFormFields.form.setAtttibute('novalidate', true);
 
 // todo: переписать на классах
-const MyForm = {
+var MyForm = {
   validate() {
     let errorFields = [],
-      isValid = true;
+        isValid = true;
 
-    const checkValidity = {
+    let checkValidity = {
       // ровно три слова
-      fio: (MyFormFields.fio.value.trim().split(/\s/).length == 3),
+      fio: (MyFormFields.fio.value.trim().split(/\s+/).length == 3),
       // RFC 5322 (только для валидации имени почты)
       email: (/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(yandex(?:\.(ru|ua|by|kz|com))|(?:ya\.ru))/.test(MyFormFields.email.value)),
       // формат +7(999)999-99-99 и сумма цифр не превышает 30
       phone: (/\+7\(\d{3}\)\d{3}-\d{2}-\d{2}/.test(MyFormFields.phone.value) &&
         MyFormFields.phone.value.replace(/\D/g, '')
-        .split('')
-        .map(val => +val)
-        .reduce((acc, val) => acc + val) <= 30),
+                                .split('')
+                                .map(val => +val)
+                                .reduce((acc, val) => acc + val) <= 30),
     };
 
     for (let key in checkValidity) {
@@ -48,7 +50,8 @@ const MyForm = {
   },
 
   setData(fields) {
-    if (fields) {
+    // в данном случае Object.prototype.toString более корректен нежели typeof
+    if (fields && (Object.prototype.toString.call(fields) === '[object Object]')) {
       MyFormFields.fio.value = fields.fio;
       MyFormFields.email.value = fields.email;
       MyFormFields.phone.value = fields.phone;
@@ -56,6 +59,21 @@ const MyForm = {
   },
 
   submit() {
+    let {isValid, errorFields} = this.validate();
 
+    let fields = this.getData();
+    Object.keys(fields).forEach(field => fields[field].classList.remove('error'));
+
+    if (isValid) {
+      MyFormFields.submit.disabled = true;
+      request();
+    } else if (errorFields.length) {
+      errorFields.forEach(field => MyFormFields[field].classList.add('error'));
+    }
   }
 };
+
+// todo: переделать под fetch API
+function request() {
+  let xhr = new XMLHttpRequest();
+}
