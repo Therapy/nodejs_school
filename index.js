7c12ac289448e2e690f95ad6a71ec4bcea943009
Therapy <therapyspb@yandex.ru>
2017-09-05 13:27:49 +0300

    . I think it doesn't matter

--- index.js
+++ index.js
@@ -21,8 +21,7 @@ var MyForm = {
       email: (/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(yandex(?:\.(ru|ua|by|kz|com))|(?:ya\.ru))/.test(Fields.email.value)),
       // формат +7(999)999-99-99 и сумма цифр не превышает 30
       phone: (/\+7\(\d{3}\)\d{3}-\d{2}-\d{2}/.test(Fields.phone.value) &&
-        Fields.phone.value.match(/\d/g)
-                          .reduce((acc, val) => acc + +val, 0) <= 30), // parseInt(val);
+        Fields.phone.value.match(/\d/g).reduce((acc, val) => acc + +val, 0) <= 30), // parseInt(val);
     };
 
     for (let key in checkValidity) {
@@ -48,7 +47,7 @@ var MyForm = {
 
   setData(fields) {
     // в данном случае Object.prototype.toString более корректен нежели typeof
-    if (fields && (Object.prototype.toString.call(fields) === '[object Object]')) {
+    if (Object.prototype.toString.call(fields) === '[object Object]') {
       Fields.fio.value = fields.fio;
       Fields.email.value = fields.email;
       Fields.phone.value = fields.phone;
